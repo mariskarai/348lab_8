db49dfa0f4fc4712399b9d8fe4a0f7a7afe0ff5a
mariskarai <rmariskaa38@gmail.com>
2025-08-26 20:09:11 -0500

    assignment

---
+++ lab1/Assignment1.py
@@ -0,0 +1,9 @@
+'''
+• Name of program contained in the file: EECS 348 Assignment 1
+• Brief description of the program: CEO Email prioritization program
+• Inputs: Assignment1_Test_File.txt
+• Output:  Next email and number of unread emails
+• Author’s full name: Mariska Rai
+• Creation date: 08/26/2025
+'''
+
